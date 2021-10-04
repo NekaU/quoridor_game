58b2db6a6efeeee6b153c8155b078dcf18ad6fd9
Atlanfa <iliatalgar@gmail.com>
2021-10-04 15:42:35 +0300

    changed move

--- move.py
+++ move.py
@@ -21,65 +21,96 @@ def places_to_move(field, player):
     players = found_players(field)
     places_to_move_list = []
     coordinates = [players[player][0], players[player][1]]
-    if coordinates[0] == 0 and coordinates[1] != 0 and coordinates[1] != 16:
-        if field[coordinates[0]][coordinates[1]+1] == 3:
-            places_to_move_list.append([coordinates[0], coordinates[1]+2])
-        if field[coordinates[0]][coordinates[1]-1] == 3:
-            places_to_move_list.append([coordinates[0], coordinates[1]-2])
-        if field[coordinates[0]+1][coordinates[1]] == 3:
-            places_to_move_list.append([coordinates[0]+2, coordinates[1]])
-    elif coordinates[0] == 16 and coordinates[1] != 0 and coordinates[1] != 16:
-        if field[coordinates[0]][coordinates[1]+1] == 3:
-            places_to_move_list.append([coordinates[0], coordinates[1]+2])
-        if field[coordinates[0]][coordinates[1]-1] == 3:
-            places_to_move_list.append([coordinates[0], coordinates[1]-2])
-        if field[coordinates[0]-1][coordinates[1]] == 3:
-            places_to_move_list.append([coordinates[0]-2, coordinates[1]])
+    if coordinates[0] == 0 and 0 < coordinates[1] < 16:
+        if check_right(field, coordinates):
+            places_to_move_list.append(right(coordinates))
+        if check_left(field, coordinates):
+            places_to_move_list.append(left(coordinates))
+        if check_down(field, coordinates):
+            places_to_move_list.append(down(coordinates))
+    elif coordinates[0] == 16 and 0 < coordinates[1] < 16:
+        if check_right(field, coordinates):
+            places_to_move_list.append(right(coordinates))
+        if check_left(field, coordinates):
+            places_to_move_list.append(left(coordinates))
+        if check_up(field, coordinates):
+            places_to_move_list.append(up(coordinates))
     elif coordinates[0] == 0 and coordinates[1] == 0:
-        if field[coordinates[0]+1][coordinates[1]] == 3:
-            places_to_move_list.append(up(coordinates[0], coordinates[1]))
-        if 
-    # elif coordinates[1] != 0 and coordinates[1] != 16:
-    #     if field[coordinates[0]][coordinates[1]+1] == 3:
-    #         places_to_move_list.append([coordinates[0], coordinates[1]+2])
-    #     if field[coordinates[0]][coordinates[1]-1] == 3:
-    #         places_to_move_list.append([coordinates[0], coordinates[1]-2])
-    #     if field[coordinates[0]-1][coordinates[1]] == 3:
-    #         places_to_move_list.append([coordinates[0]-2, coordinates[1]])
-    #     if field[coordinates[0]+1][coordinates[1]] == 3:
-    #         places_to_move_list.append([coordinates[0]+2, coordinates[1]])
-    # elif coordinates[1] == 0:
-    #     if field[coordinates[0]][coordinates[1]+1] == 3:
-    #         places_to_move_list.append([coordinates[0], coordinates[1]+2])
-    #     if field[coordinates[0]+1][coordinates[1]] == 3:
-    #         places_to_move_list.append([coordinates[0]+2, coordinates[1]])
-    #     if field[coordinates[0]-1][coordinates[1]] == 3:
-    #         places_to_move_list.append([coordinates[0]-2, coordinates[1]])
-    # elif coordinates[1] == 16:
-    #     if field[coordinates[0]][coordinates[1]-1] == 3:
-    #         places_to_move_list.append([coordinates[0], coordinates[1]+2])
-    #     if field[coordinates[0]+1][coordinates[1]] == 3:
-    #         places_to_move_list.append([coordinates[0]+2, coordinates[1]])
-    #     if field[coordinates[0]-1][coordinates[1]] == 3:
-    #         places_to_move_list.append([coordinates[0]-2, coordinates[1]])
+        if check_up(field, coordinates):
+            places_to_move_list.append(up(coordinates))
+        if check_right(field, coordinates):
+            places_to_move_list.append(right(coordinates))
+    elif coordinates[0] == 0 and coordinates[1] == 16:
+        if check_up(field, coordinates):
+            places_to_move_list.append(up(coordinates))
+        if check_left(field, coordinates):
+            places_to_move_list.append(left(coordinates))
+    elif coordinates[0] == 16 and coordinates[1] == 0:
+        if check_down(field, coordinates):
+            places_to_move_list.append(down(coordinates))
+        if check_right(field, coordinates):
+            places_to_move_list.append(right(coordinates))
+    elif coordinates[0] == 16 and coordinates[1] == 16:
+        if check_up(field, coordinates):
+            places_to_move_list.append(up(coordinates))
+        if check_left(field, coordinates):
+            places_to_move_list.append(left(coordinates))
+    elif 0 < coordinates[0] < 16 and coordinates[1] == 16:
+        if check_down(field, coordinates):
+            places_to_move_list.append(down(coordinates))
+        if check_up(field, coordinates):
+            places_to_move_list.append(up(coordinates))
+        if check_left(field, coordinates):
+            places_to_move_list.append(left(coordinates))
+    elif 0 < coordinates[0] < 16 and coordinates[1] == 0:
+        if check_down(field, coordinates):
+            places_to_move_list.append(down(coordinates))
+        if check_up(field, coordinates):
+            places_to_move_list.append(up(coordinates))
+        if check_right(field, coordinates):
+            places_to_move_list.append(right(coordinates))
+    elif 0 < coordinates[0] < 16 and 0 < coordinates[1] < 16:
+        if check_down(field, coordinates):
+            places_to_move_list.append(down(coordinates))
+        if check_up(field, coordinates):
+            places_to_move_list.append(up(coordinates))
+        if check_right(field, coordinates):
+            places_to_move_list.append(right(coordinates))
+        if check_left(field, coordinates):
+            places_to_move_list.append(left(coordinates))
     return places_to_move_list
 
 
-def up(i, j):
-    return [i-2, j]
+def up(coordinates):
+    return [coordinates[0]-2, coordinates[1]]
 
 
-def down(i, j):
-    return [i+2, j]
+def down(coordinates):
+    return [coordinates[0]+2, coordinates[1]]
 
 
-def left(i, j):
-    return [i, j-2]
+def left(coordinates):
+    return [coordinates[0], coordinates[1]-2]
 
 
-def right(i, j):
-    return [i, j+2]
+def right(coordinates):
+    return [coordinates[0], coordinates[1]+2]
 
+
+def check_up(field, coordinates):
+    return True if field[coordinates[0]-1][coordinates[1]] == 3 else False
+
+
+def check_down(field, coordinates):
+    return True if field[coordinates[0]+1][coordinates[1]] == 3 else False
+
+
+def check_right(field, coordinates):
+    return True if field[coordinates[0]][coordinates[1]+1] == 3 else False
+
+
+def check_left(field, coordinates):
+    return True if field[coordinates[0]][coordinates[1]-1] == 3 else False
 
 # print(places_to_move(field_preparation(fill_the_field()), "first"))
 # print(places_to_move(field_preparation(fill_the_field()), "second"))
