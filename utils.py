2a59fd78b2f4105cb61b8c287fbcb709c95e0a5c
Atlanfa <iliatalgar@gmail.com>
2021-10-08 14:04:27 +0300

    deleting old version

--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def clear_console():
-    command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-        command = 'cls'
-    os.system(command)
